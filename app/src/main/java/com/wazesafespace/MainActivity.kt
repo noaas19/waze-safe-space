5930dc029deee8e43b83583e704fd2a36d0dcdf9
noaas19 <noaasulin106@gmail.com>
2024-06-17 13:31:17 +0300

    add firebase dependency

--- app/src/main/java/com/wazesafespace/MainActivity.kt
+++ app/src/main/java/com/wazesafespace/MainActivity.kt
@@ -11,12 +11,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 import com.wazesafespace.ui.theme.WazeSafeSpaceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Initialize Firebase SDK
+        FirebaseApp.initializeApp(this)
+
+        // Initialize Firebase Firestore
+        val db = FirebaseFirestore.getInstance()
+
+        // Initialize Firebase Cloud Messaging (FCM)
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+
+        // Initialize Firebase Analytics
+        val analytics = FirebaseAnalytics.getInstance(this)
+
         setContent {
             WazeSafeSpaceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
